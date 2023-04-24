24f7a75805d77a7892d50490edb9ba34eeb42e65
saidevops-training <119907175+saidevops-training@users.noreply.github.com>
2023-04-24 19:46:09 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,7 @@
 public class HelloWorld{
   public static void main (String args[]){
     System.out.println("Welcome to java!");
-    System.out.println("Change from Sai computer");
-    System.out.println("Change from Eshwar Krishna");
-    System.out.println("Change from Eshwar computer");
-    System.out.println("New change from Sai");
     System.out.println("New change from Sai desktop on 24 april");
+    System.out.println("New change from Sai Github account on 24 april");
   }
 }
